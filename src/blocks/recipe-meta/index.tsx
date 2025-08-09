9c1b632b0f45270a7cc5842aa7d7f9158473554e
KJ Roelke <kj.roelke@gmail.com>
2025-08-09 10:43:47 -0500

    Build RecipeMacros block Fixes #3

---
+++ src/blocks/recipe-meta/index.tsx
@@ -0,0 +1,23 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import { receipt } from '@wordpress/icons';
+import metadata from './block.json';
+import Edit from './edit';
+
+registerBlockType( metadata.name, {
+	icon: receipt,
+	edit: Edit,
+	save: ( { attributes } ) => {
+		const { protein, carbs, fat, calories } = attributes;
+		return (
+			<div { ...useBlockProps.save() }>
+				<ul>
+					<li>Protein: { protein }g</li>
+					<li>Carbs: { carbs }g</li>
+					<li>Fat: { fat }g</li>
+					<li>Calories: { calories }</li>
+				</ul>
+			</div>
+		);
+	},
+} );
