eacb09750381ba4adfb46eafc66aa2499538b0db
KJ Roelke <kj.roelke@gmail.com>
2025-08-30 12:45:16 -0500

    update plugin file header

--- meal-plannr.php
+++ meal-plannr.php
@@ -3,6 +3,9 @@
  * Plugin Name: Meal Plannr
  * Description: A plugin to help you plan your meals.
  * Version: 1.0.0
+ * Requires at least: 6.7.0
+ * Tested up to: 6.8.2
+ * Requires PHP: 8.2
  * Author: K.J. Roelke
  *
  * @package MealPlannr
